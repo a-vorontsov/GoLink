03de5f446c0e0e617f4d6c5d20eb1720e15908ce
Kelvin Zhang <me@iamkelv.in>
2016-07-16 14:24:09 +0100

    Refactor messages into a directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -87,5 +87,15 @@ angular.module('app.directives', [])
         });
       }
     }
+  })
+
+  .directive('message', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        data: '=data'
+      },
+      templateUrl: 'templates/directives/message.html'
+    }
   });
 
