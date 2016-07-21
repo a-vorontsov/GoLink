2b9aa75d46fd75a59b8d8bcfb4060fd69d037f9d
Kelvin Zhang <me@iamkelv.in>
2016-07-21 13:09:12 +0100

    Remove angularfire

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ var paths = {
   ionicFonts: ['./www/lib/ionic/fonts/*'],
   images: ['./www/img/**/*'],
   lib: [
-    './www/lib/angularfire/dist/angularfire.js',
     './www/lib/firebase/firebase.js',
     './www/lib/geofire/dist/geofire.min.js',
     './www/lib/ionic-image-lazy-load/ionic-image-lazy-load.js',
