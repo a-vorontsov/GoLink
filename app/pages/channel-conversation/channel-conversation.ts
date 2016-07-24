2a08eebbbe0cff4614391ad305a61b606e628289
Kelvin Zhang <me@iamkelv.in>
2016-07-24 15:51:53 +0100

    Create boilerplate channel code

---
+++ app/pages/channel-conversation/channel-conversation.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ChannelConversationPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/channel-conversation/channel-conversation.html',
+})
+export class ChannelConversationPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
---
+++ app/pages/channels/channels.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the ChannelsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/channels/channels.html',
+})
+export class ChannelsPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
