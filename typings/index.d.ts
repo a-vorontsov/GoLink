4d11bbd8c20a8318315e9a3ab8c3e71bb44bc01f
Kelvin Zhang <me@iamkelv.in>
2016-07-24 16:25:53 +0100

    Add firebase to typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="modules/firebase/index.d.ts" />
+
 declare var firebase: any;
 declare var moment: any;
 declare var GeoFire: any;
